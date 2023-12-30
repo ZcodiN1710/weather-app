3fe404e616b45c9ce091525538f927233323bb13
ZcodiN1710 <zakariandao01@gmail.com>
2023-12-30 22:56:22 +0400

    modified

--- README.md
+++ README.md
@@ -1,1 +1,57 @@
-# weather-app
+# Weather-Journal App
+
+## Overview
+
+This project is an asynchronous web app that utilizes a Web API to dynamically update the user interface with weather information. It allows users to input their zip code and feelings, and in return, it fetches real-time weather data using a Web API. The UI is then updated dynamically to display the weather information along with the user's feelings and the current date.
+
+## Features
+
+- Asynchronous JavaScript
+- Web API Integration for Weather Data
+- Dynamic UI Update
+- User Input Handling
+
+## Prerequisites
+
+Before you begin, ensure you have the following:
+
+- Node.js installed on your machine
+- API key for the weather data provider (e.g., OpenWeatherMap)
+
+## Getting Started
+
+1. Clone the repository:
+
+   ```bash
+   git clone https://github.com/ZcodiN1710/weather-app
+    1. Navigate to the project directory:
+        cd weather-appp
+    2.Install dependencies:
+        npm install
+    3.Start the server:
+        npm start
+    4.Open your browser and go to http://localhost:3000 to use the Weather-Journal App.
+
+## Usage
+
+    Enter your zip code.
+    Input your feelings or mood.
+    Click the "Generate" button.
+    The app will fetch the current weather data and update the UI with the information.
+
+## Technologies Used
+
+    HTML5
+    CSS3
+    JavaScript (Vanilla)
+    Node.js
+    Express.js
+    Web API (OpenWeatherMap)
+
+## Acknowledgments
+
+    Thanks to OpenWeatherMap for providing the weather data API.
+
+## License
+
+    Feel free to customize this template according to the specific details and re
--- web-app/app.js
+++ web-app/app.js
@@ -1,13 +1,14 @@
-const zipCode = document.getElementById("zip").value;
 const generateBtn = document.getElementById("generate");
+const zipInput = document.getElementById("zip");
+const baseUrl = `http://api.openweathermap.org/data/2.5/forecast?zip=`;
+const apiKey = "&appid=576c33c43d66e9e66d770b967c83cc76&units=imperial";
 
-const baseUrl = `http://api.openweathermap.org/data/2.5/forecast?zip=${zipCode}&appid=`;
-const apiKey = "576c33c43d66e9e66d770b967c83cc76&units=imperial";
-const fullApiUrl = baseUrl + apiKey;
-console.log(fullApiUrl);
 
 // =============get the api============================
 const getApiData = async (fullApiUrl) => {
+  let zipCode = zipInput.value
+  fullApiUrl =`http://api.openweathermap.org/data/2.5/forecast?zip=${zipCode}${apiKey}`
+  console.log(fullApiUrl);
   const response = await fetch(fullApiUrl);
   try {
     const data = await response.json();
@@ -46,29 +47,30 @@ const performActions = () => {
   const userResponse = document.getElementById("feelings").value;
   console.log(userResponse);
   // get the temperature
-  getApiData(fullApiUrl).then((temp) => {
+  getApiData().then((temp) => {
     console.log(temp);
     postData("/addData", {
       temperature: temp,
       date: currentDate,
       feeling: userResponse,
     });
+    updateUI(currentDate,temp,userResponse)
   });
-  updateUI()
+  
 };
 //==================update the UI==========================
-const updateUI = async () => {
-  const request = await fetch("/all");
-  try {
-    const allData = await request.json();
-    console.log(allData);
-    const dateEl = (document.getElementById("date").innerHTML =
-      allData[0].date);
-    const tempEl = (document.getElementById("temp").innerHTML =
-      allData[0].temperature);
-    const contentEl = (document.getElementById("content").innerHTML =
-      allData[0].feeling);
-  } catch (error) {}
+const updateUI =  (date,temperature,feeling) => {
+  // const request = await fetch("/all");
+  // try {
+    // const allData = await request.json();
+    // console.log(allData);
+    const dateEl = document.querySelector("#date p").innerHTML =
+      date;
+    const tempEl = document.querySelector("#temp p").innerHTML =
+    temperature;
+    const contentEl = document.querySelector("#content").innerHTML =
+      feeling
+  // } catch (error) {}
 };
 //  ==========listen to click generate button===========================
 generateBtn.addEventListener("click", performActions);
